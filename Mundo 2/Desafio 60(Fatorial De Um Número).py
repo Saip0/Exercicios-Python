d9ec0d211cfd07d325abe70e022b733e4d7aa63e
Sam <samuelgomesdf1@gmail.com>
2026-03-05 18:25:05 -0300

    Update Desafio 60(Fatorial De Um Número).py

--- Mundo 2/Desafio 60(Fatorial De Um Número).py
+++ Mundo 2/Desafio 60(Fatorial De Um Número).py
@@ -1,3 +1,4 @@
+#Um Programa Simples Apenas Para Treinar A Lógica
 while True:
     try:
         num1 = int(input("Numero:"))
@@ -7,4 +8,5 @@ while True:
     fact = 1
     for i in range(1,num1+1):
         fact *= i
+
     print(fact)
