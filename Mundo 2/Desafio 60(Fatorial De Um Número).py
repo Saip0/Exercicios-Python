729e0442a873dcee0c198454cfa6d825f3913836
Sam <samuelgomesdf1@gmail.com>
2026-03-05 18:24:10 -0300

    Add files via upload

---
+++ Mundo 2/Desafio 60(Fatorial De Um Número).py
@@ -0,0 +1,10 @@
+while True:
+    try:
+        num1 = int(input("Numero:"))
+    except ValueError:
+        print("Valor invalido")
+        continue
+    fact = 1
+    for i in range(1,num1+1):
+        fact *= i
+    print(fact)
